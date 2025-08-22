0b151cba6e8c6febc06831d90b207943dd79d1b4
icepik77 <icepik77@mail.ru>
2025-08-22 11:35:16 +0300

    opened the new routers

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 // Маршруты
-// import authRoutes from "./routes/auth";
-// app.use("/api/auth", authRoutes);
+import authRoutes from "./routes/auth";
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
